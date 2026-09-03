1cafb8a4ff8a00e80a82b0caef6b7f93ce0bc486
Jannis <j_dickel@web.de>
2026-09-03 20:50:28 +0200

    feat: add canvas marquee

---
+++ marquee.js
@@ -0,0 +1,42 @@
+  const canvas = document.getElementById('marquee');
+  const ctx = canvas.getContext('2d');
+ 
+  const text = "© 2026 Jannis Dickel. A DIGITAL TRIBUTE. NOT AFFILIATED WITH BMW AG.";
+  const fontSize = 10;
+  const speed = 0.5;
+ 
+  ctx.font = `${fontSize}px monospace`;
+  let textWidth = ctx.measureText(text).width;
+  let x = -textWidth;
+ 
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+ 
+    ctx.textBaseline = 'middle'
+    ctx.fillStyle = '#c2c6d5'
+    ctx.fillText(text, x, canvas.height / 2);
+ 
+    x += speed; 
+ 
+    if (x > canvas.width) {
+        x = -textWidth;
+    }
+    
+    requestAnimationFrame(draw);
+  }
+ 
+  draw();
+
+
+function resizeCanvas() {
+  const dpr = window.devicePixelRatio;
+  const rect = canvas.getBoundingClientRect();
+
+  canvas.width = rect.width * dpr;
+  canvas.height = rect.height * dpr;
+
+  ctx.setTransform(dpr, 0, 0, dpr, 0, 0);
+}
+
+resizeCanvas();
+window.addEventListener('resize', resizeCanvas);
